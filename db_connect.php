4b8b63ccf7f364cc6da502d1512325fbade15027
BazingaLegend05 <jaymcleod2005@gmail.com>
2026-04-29 21:27:46 +0000

    Created dev container for database editing. also added log in functionality

---
+++ db_connect.php
@@ -0,0 +1,17 @@
+<?php
+$host = 'db';
+$dbname = 'mariadb';
+$username = 'mariadb';
+$password = 'mariadb';
+
+try {
+    $dsn = "mysql:host=$host;dbname=$dbname;charset=utf8mb4";
+    $pdo = new PDO($dsn, $username, $password);
+    
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    
+    echo "Connected successfully to the database!";
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
+?>
